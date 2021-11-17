7c8131b97c28be062fae69576a18a0e5cb9c345f
robmikh <rob.mikh@outlook.com>
2021-11-16 23:42:23 -0800

    update to windows v0.25

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,33 @@
 mod interop;
 
-use bindings::windows::{
-    application_model::data_transfer::{Clipboard, DataPackage, DataPackageOperation},
-    storage::KnownFolders,
-    system::{DispatcherQueue, DispatcherQueueHandler},
-    win32::{
-        keyboard_and_mouse_input::GetKeyState,
-        system_services::{HINSTANCE, LRESULT, VK_LWIN, WH_KEYBOARD_LL, WM_KEYUP},
-        windows_and_messaging::{
-            CallNextHookEx, DispatchMessageA, GetMessageA, SetWindowsHookExA, UnhookWindowsHookEx,
-            HWND, KBDLLHOOKSTRUCT, LPARAM, MSG, WPARAM,
-        },
-        winrt::{RoInitialize, RO_INIT_TYPE},
-    },
-};
 use interop::create_dispatcher_queue_controller_for_current_thread;
 use std::fs::{read_dir, File};
 use std::path::Path;
 use std::time::SystemTime;
-use std::{
-    io::Read,
-    path::PathBuf,
-    sync::Once,
+use std::{io::Read, path::PathBuf, sync::Once};
+use windows::{
+    runtime::{Handle, Result},
+    ApplicationModel::DataTransfer::{Clipboard, DataPackage, DataPackageOperation},
+    Storage::KnownFolders,
+    System::{DispatcherQueue, DispatcherQueueHandler},
+    Win32::{
+        Foundation::{HINSTANCE, HWND, LPARAM, LRESULT, WPARAM},
+        System::WinRT::{RoInitialize, RO_INIT_SINGLETHREADED},
+        UI::{
+            Input::KeyboardAndMouse::{GetKeyState, VK_LWIN},
+            WindowsAndMessaging::{
+                CallNextHookEx, DispatchMessageA, GetMessageA, SetWindowsHookExA,
+                UnhookWindowsHookEx, HHOOK, KBDLLHOOKSTRUCT, MSG, WH_KEYBOARD_LL, WM_KEYUP,
+            },
+        },
+    },
 };
 
 static mut MAIN_THREAD_QUEUE: Option<DispatcherQueue> = None;
 static INIT: Once = Once::new();
 
 #[repr(transparent)]
-struct HHook(isize);
+struct HHook(HHOOK);
 
 impl Drop for HHook {
     fn drop(&mut self) {
@@ -38,26 +37,19 @@ impl Drop for HHook {
     }
 }
 
-fn main() -> windows::Result<()> {
+fn main() -> Result<()> {
     unsafe {
-        RoInitialize(RO_INIT_TYPE::RO_INIT_SINGLETHREADED).ok()?;
+        RoInitialize(RO_INIT_SINGLETHREADED)?;
     }
 
     let controller = create_dispatcher_queue_controller_for_current_thread()?;
-    let queue = controller.dispatcher_queue()?;
+    let queue = controller.DispatcherQueue()?;
     INIT.call_once(|| unsafe {
         MAIN_THREAD_QUEUE = Some(queue);
     });
 
-    let hook = unsafe {
-        HHook(SetWindowsHookExA(
-            WH_KEYBOARD_LL,
-            Some(hook_proc),
-            HINSTANCE(0),
-            0,
-        ))
-    };
-    assert!(hook.0 != 0);
+    let _hook =
+        unsafe { HHook(SetWindowsHookExA(WH_KEYBOARD_LL, Some(hook_proc), HINSTANCE(0), 0).ok()?) };
 
     unsafe {
         let mut message = MSG::default();
@@ -69,12 +61,12 @@ fn main() -> windows::Result<()> {
     Ok(())
 }
 
-fn copy_code() -> windows::Result<()> {
-    let documents_folder = KnownFolders::documents_library()?;
+fn copy_code() -> Result<()> {
+    let documents_folder = KnownFolders::DocumentsLibrary()?;
     let save_folder = documents_folder
-        .get_folder_async("Warcraft III\\CustomMapData\\DRT1")?
+        .GetFolderAsync("Warcraft III\\CustomMapData\\DRT1")?
         .get()?;
-    let save_path = save_folder.path()?.to_string();
+    let save_path = save_folder.Path()?.to_string();
     let save_path = Path::new(&save_path);
     if !save_path.exists() {
         println!(
@@ -88,9 +80,9 @@ fn copy_code() -> windows::Result<()> {
 
     // copy the code to the clipboard
     let package = DataPackage::new()?;
-    package.set_requested_operation(DataPackageOperation::Copy)?;
-    package.set_text(code)?;
-    Clipboard::set_content(package)?;
+    package.SetRequestedOperation(DataPackageOperation::Copy)?;
+    package.SetText(code)?;
+    Clipboard::SetContent(package)?;
     println!("Code copied to clipboard!");
 
     Ok(())
@@ -136,31 +128,29 @@ extern "system" fn hook_proc(code: i32, w_param: WPARAM, l_param: LPARAM) -> LRE
     unsafe {
         if code == 0 {
             // w_param holds the identifier of the keyboard message
-            if w_param.0 as i32 == WM_KEYUP {
+            if w_param.0 as u32 == WM_KEYUP {
                 // l_param holds a pointer to a KBDLLHOOKSTRUCT struct
                 let keyboard_info: *mut KBDLLHOOKSTRUCT = std::mem::transmute(l_param);
                 let keyboard_info = keyboard_info.as_ref().unwrap();
 
                 // 0x4A is the J key
-                if keyboard_info.vk_code == 0x4A {
+                if keyboard_info.vkCode == 0x4A {
                     // Check to see if the windows key is also down
-                    let key_state = GetKeyState(VK_LWIN);
+                    let key_state = GetKeyState(VK_LWIN.0 as i32);
                     if key_state != 0 {
                         // Signal the main thread to find and copy the new save code
                         let main_thread_queue = MAIN_THREAD_QUEUE.clone().unwrap();
                         main_thread_queue
-                            .try_enqueue(DispatcherQueueHandler::new(
-                                move || -> windows::Result<()> {
-                                    copy_code()?;
-                                    Ok(())
-                                },
-                            ))
+                            .TryEnqueue(DispatcherQueueHandler::new(move || -> Result<()> {
+                                copy_code()?;
+                                Ok(())
+                            }))
                             .unwrap();
                     }
                 }
             }
         }
 
-        return CallNextHookEx(0, code, w_param, l_param);
+        return CallNextHookEx(HHOOK(0), code, w_param, l_param);
     }
 }
